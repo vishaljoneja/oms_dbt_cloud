44a49bf96748d0e61fc6f23578744f89db4930bd
Vishal Joneja <Vishal.Joneja@gmail.com>
2026-01-12 14:28:30 +0000

    testing

---
+++ models/customers_stg.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table')}}
+
+select
+    customerid,
+    firstname,
+    lastname,
+    email,
+    phone,
+    address,
+    city,
+    state,
+    zipcode,
+    updated_at,
+    concat(firstname, ' ', lastname) as customername
+from 
+    {{ source('landing','customers')}}
